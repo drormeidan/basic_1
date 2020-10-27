b3bd50d7aa592649fa098bdd8012006a79e451b3
Dror Meidan <meidan.dror@gmail.com>
2020-10-27 12:30:43 +0200

    level7

---
+++ Worker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Worker
+{
+	public Worker(string sname, int sseniority)
+	{
+		this.name = sname;
+		this.seniority = sseniority;
+		this.NumInvetation = 0;
+	}
+
+	public string name { get; set; }
+	public int seniority { get; set; }
+	public int NumInvetation { get; set; }
+
+	public int salary() 
+	{
+		return (10 * this.seniority) + this.NumInvetation;
+	}
+
+}
