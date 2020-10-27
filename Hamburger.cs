4a14c28915b932376ee2550466460c2b80986fa8
Dror Meidan <meidan.dror@gmail.com>
2020-10-27 15:09:59 +0200

    level9

--- Hamburger.cs
+++ Hamburger.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 
 public class Hamburger
 {
-	public Hamburger(int smeat, List<int> sextras, int sbread)
+	public Hamburger(int smeat, List<int> sextras, int sbread, List<Sauce> ssauces)
 	{
 		this.meat = smeat;
 		this.extras = sextras;
 		this.bread = sbread;
+		this.SauceList = ssauces;
 	}
+	public List<Sauce> SauceList { get; set; }
 	public int meat { get; set; } // 0-meat, 1-lamb, 2-tofo
 	public List<int> extras { get; set; } // 0-cucamber, 1-tomato, 2-lettuce, 3-pickels, 4-onion
 	public int bread { get; set; } // 0- brown, 1-white
@@ -38,17 +40,34 @@ public class Hamburger
 			rprice += ExtraPrices[IdxExtra[Iextra]];
 		}
 
+		foreach (Sauce sau in this.SauceList) 
+		{
+			rprice += sau.price;
+		}
+
 		return rprice;
 			
 	}
 
-	public void ReduceAmmount(ref Dictionary<int, string> IdxExtra, ref Dictionary<int, string> IdxMeat, ref Dictionary<int, string> IdxBread, ref Dictionary<string, int> ExtraAmount, ref Dictionary<string, int> MeatAmount, ref Dictionary<string, int> BreadAmount) 
+	public void ReduceAmmount(ref List<Sauce> sauces, ref Dictionary<int, string> IdxExtra, ref Dictionary<int, string> IdxMeat, ref Dictionary<int, string> IdxBread, ref Dictionary<string, int> ExtraAmount, ref Dictionary<string, int> MeatAmount, ref Dictionary<string, int> BreadAmount) 
 	{
 		MeatAmount[IdxMeat[this.meat]] += -1;
 		BreadAmount[IdxBread[this.bread]] += -1;
 		foreach(int item in this.extras) 
 		{
 			ExtraAmount[IdxExtra[item]] += -1;
+		}
+
+		foreach(Sauce item in this.SauceList) 
+		{
+			foreach (Sauce sau in sauces)
+			{
+				if (item.idx == sau.idx)
+				{
+					sau.amount += -1; 
+				}
+			}
+
 		}
 	}
 }
